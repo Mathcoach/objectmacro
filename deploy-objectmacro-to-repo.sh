6856497ee9896fa46fdfaa3a6a20f94adda6a3b0
Hong-Phuc Bui <hong-phuc.bui@htwsaar.de>
2022-07-17 00:17:28 +0200

    use pom.xml

--- deploy-objectmacro-to-repo.sh
+++ deploy-objectmacro-to-repo.sh
@@ -1,10 +1,8 @@
 #!/bin/sh
 set -x
-mvn deploy:deploy-file -DgroupId=de.htwsaar.mc\
-  -DartifactId=objectmacro \
-  -Dversion='4-beta.4' \
-  -Dpackaging=jar \
-  -Dfile=sablecc-4-beta.4/lib/objectmacro.jar \
-  -DrepositoryId=central\
-  -Durl='https://bayes.htwsaar.de/nexus/repository/maven-releases/'
+mvn deploy:deploy-file \
+  -DpomFile=pom.xml \
+  -Dfile=sablecc-4-beta.4/lib/objectmacro.jar\
+  -DrepositoryId=central \
+  -Durl='https://bayes.htwsaar.de/nexus/repository/maven-snapshots/'
 
