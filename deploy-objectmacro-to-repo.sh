c42f2bfd9aa2e5202120df650281b72eefe8ed28
Hong-Phuc Bui <hong-phuc.bui@htwsaar.de>
2022-07-19 13:46:09 +0200

    test

--- deploy-objectmacro-to-repo.sh
+++ deploy-objectmacro-to-repo.sh
@@ -4,5 +4,5 @@ mvn gpg:sign-and-deploy-file \
   -DpomFile=pom.xml \
   -Dfile=sablecc-4-beta.4/lib/objectmacro.jar\
   -DrepositoryId=central \
-  -Durl='https://bayes.htwsaar.de/nexus/repository/maven-snapshots/'
+  -Durl='https://bayes.htwsaar.de/nexus/repository/maven-releases/'
 
