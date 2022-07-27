600fcd41a7f878cd0d9c8d7380f24f9fe1fa5a71
Hong-Phuc Bui <hong-phuc.bui@htwsaar.de>
2022-07-27 22:37:47 +0200

    add script (template Status) to deploy to maven central

---
+++ deploy-objectmacro-to-mavencentral.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+set -x
+mvn gpg:sign-and-deploy-file \
+  -DpomFile=pom.xml \
+  -Dfile=sablecc-4-beta.4/lib/objectmacro.jar\
+  -DrepositoryId=ossrh\
+  -Durl='https://bayes.htwsaar.de/nexus/repository/maven-releases/'
+
