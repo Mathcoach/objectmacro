f33f60d1a6e9cd27bb3a04486189b7fbccc3cee1
Hong-Phuc Bui <hong-phuc.bui@htwsaar.de>
2022-07-28 15:42:27 +0200

    add sources and javadoc to maven deploy script

--- deploy-objectmacro-to-mavencentral.sh
+++ deploy-objectmacro-to-mavencentral.sh
@@ -1,8 +1,12 @@
 #!/bin/sh
 set -x
+mvn source:jar
+mvn javadoc:javadoc; mvn javadoc:jar
 mvn gpg:sign-and-deploy-file \
   -DpomFile=pom.xml \
   -Dfile=sablecc-4-beta.4/lib/objectmacro.jar\
+  -Dsources=./target/objectmacro-4-beta.4-sources.jar\
+  -Djavadoc=./target/objectmacro-4-beta.4-javadoc.jar\
   -DrepositoryId=ossrh\
   -Durl='https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/'
 
