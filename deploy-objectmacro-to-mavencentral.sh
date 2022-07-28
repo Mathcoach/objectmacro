5dc6bd97240832290cd834423f4176270664f358
Hong-Phuc Bui <hong-phuc.bui@htwsaar.de>
2022-07-28 10:24:49 +0200

    work well

--- deploy-objectmacro-to-mavencentral.sh
+++ deploy-objectmacro-to-mavencentral.sh
@@ -4,5 +4,5 @@ mvn gpg:sign-and-deploy-file \
   -DpomFile=pom.xml \
   -Dfile=sablecc-4-beta.4/lib/objectmacro.jar\
   -DrepositoryId=ossrh\
-  -Durl='https://bayes.htwsaar.de/nexus/repository/maven-releases/'
+  -Durl='https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/'
 
